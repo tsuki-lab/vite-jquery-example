3012ccc02d31e03af1e663caa743d9e1acecc45e
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-01-14 11:20:30 +0900

    🔧 Adapt jQuery v3.6.0

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+$(function(){
+  const htmlText = `
+    <h1>Hello Vite + jQuery!</h1>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  `
+
+  $('#app').html(htmlText)
+});
